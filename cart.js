539bfd0a1af559ec4a165acfcd880cf929996641
Shahina <shahina@qburst.com>
2017-02-20 16:01:18 +0530

    Demo site

--- cart.js
+++ cart.js
@@ -1,37 +1,7 @@
-//let KO know that we will take care of managing the bindings of our children
-ko.bindingHandlers.stopBinding = {
-    init: function () {
-        return {
-            controlsDescendantBindings: true
-        };
-    }
-};
-
-//KO 2.1, now allows you to add containerless support for custom bindings
-ko.virtualElements.allowedBindings.stopBinding = true;
-
-var homeModel = {
-    selectedSection: ko.observable("home")
-};
-
-var selectBrandModel = {
-    selectBrand: function (event) {
-        this.products = ko.observable(event.products);
-        homeModel.selectedSection("selectMobile");
-            
-    }
-};
-
-var selectMobileModel = {
-    selectMobile: function () {
-        alert("Saved");
-    }
-};
-
 function formatCurrency(value) {
-    return "$";
+    return "$" + value.toFixed(2);
 }
- 
+
 var CartLine = function() {
     var self = this;
     self.category = ko.observable();
@@ -46,117 +16,58 @@ var CartLine = function() {
         self.product(undefined);
     });
 };
- 
-var Cart = function() {
+
+var shouter = new ko.subscribable();
+var selectBrandModel = function(){
+    this.showBrands = ko.observable(true);    
+
+    this.lines = ko.observableArray();
+    this.selectBrand = function(event) {       
+       this.lines.push(event);         
+    }.bind(this);
+
+    this.lines.subscribe(function(newValue) {
+        shouter.notifySubscribers(newValue, "productsToPublish");
+        selectBrandModel.showBrands(false);
+    });
+   
+};
+
+var selectMobileModel = function(){    
+    this.showMobiles = ko.observable(false);  
+
+    this.productsArray=ko.observableArray();
+    shouter.subscribe(function(newValue) {
+        this.productsArray(newValue);
+        selectMobileModel.showMobiles(true);
+    }, this, "productsToPublish");
+    console.log("sdsdsdsdsdsds "+this.productsArray);
     // Stores an array of lines, and from these, can work out the grandTotal
     var self = this;
-    debugger;
     self.lines = ko.observableArray([new CartLine()]); // Put one line in by default
     self.grandTotal = ko.computed(function() {
         var total = 0;
-        $.each(self.lines(), function() { 
-            total += this.subtotal();
-        });
+        $.each(self.lines(), function() { total += this.subtotal() })
         return total;
-    });    
+    });
  
     // Operations
-    self.addLine = function() { 
-        self.lines.push(new CartLine());
-    };
-
-    self.removeLine = function(line) { 
-        self.lines.remove(line);
-    };
-
-    self.totalItems = ko.computed(function() {
-        var count = 0;
-        $.each(self.lines(), function() {            
-            if(this.subtotal() > 0) {
-                count += parseInt(this.quantity());
-            }            
-        });
-        return count;
-    });
-
+    self.addLine = function() { self.lines.push(new CartLine()) };
+    self.removeLine = function(line) { self.lines.remove(line) };
     self.save = function() {
         var dataToSave = $.map(self.lines(), function(line) {
             return line.product() ? {
                 productName: line.product().name,
                 quantity: line.quantity()
-            } : undefined;
+            } : undefined
         });
-        alert("Saved");
+        alert("Could now send this to server: " + JSON.stringify(dataToSave));
     };
 };
 
+var homeModel = (function(){
+    this.selectBrandModel =  new selectBrandModel(),
+    this.selectMobileModel = new selectMobileModel();
+})();
 
 ko.applyBindings(homeModel);
-ko.applyBindings(selectBrandModel, document.getElementById("home"));
-ko.applyBindings(selectMobileModel, document.getElementById("selectMobile"));
-
-
-// function formatCurrency(value) {
-//     return "$" + value.toFixed(2);
-// }
- 
-// var CartLine = function() {
-//     var self = this;
-//     self.category = ko.observable();
-//     self.product = ko.observable();
-//     self.quantity = ko.observable(1);
-//     self.subtotal = ko.computed(function() {
-//         return self.product() ? self.product().price * parseInt("0" + self.quantity(), 10) : 0;
-//     });
- 
-//     // Whenever the category changes, reset the product selection
-//     self.category.subscribe(function() {
-//         self.product(undefined);
-//     });
-// };
- 
-// var Cart = function() {
-//     // Stores an array of lines, and from these, can work out the grandTotal
-//     var self = this;
-//     self.lines = ko.observableArray([new CartLine()]); // Put one line in by default
-//     self.grandTotal = ko.computed(function() {
-//         var total = 0;
-//         $.each(self.lines(), function() { 
-//             total += this.subtotal();
-//         });
-//         return total;
-//     });    
- 
-//     // Operations
-//     self.addLine = function() { 
-//         self.lines.push(new CartLine());
-//     };
-
-//     self.removeLine = function(line) { 
-//         self.lines.remove(line);
-//     };
-
-//     self.totalItems = ko.computed(function() {
-//         var count = 0;
-//         $.each(self.lines(), function() {            
-//             if(this.subtotal() > 0) {
-//                 count += parseInt(this.quantity());
-//             }            
-//         });
-//         return count;
-//     });
-
-//     self.save = function() {
-//         var dataToSave = $.map(self.lines(), function(line) {
-//             return line.product() ? {
-//                 productName: line.product().name,
-//                 quantity: line.quantity()
-//             } : undefined;
-//         });
-//         alert("Saved");
-//     };
-// };
-
-
-// // Activates knockout.js
-// ko.applyBindings(new Cart());
